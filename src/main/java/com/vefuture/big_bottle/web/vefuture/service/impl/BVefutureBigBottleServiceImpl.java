59cc9a02b999e21fd7e2960f82950e6b72201a80
wangb <870719738@qq.com>
2025-03-30 21:00:20 +0900

    feat: 地址统一用小写

--- src/main/java/com/vefuture/big_bottle/web/vefuture/service/impl/BVefutureBigBottleServiceImpl.java
+++ src/main/java/com/vefuture/big_bottle/web/vefuture/service/impl/BVefutureBigBottleServiceImpl.java
@@ -89,7 +89,12 @@ public class BVefutureBigBottleServiceImpl extends ServiceImpl<BVefutureBigBottl
     @Override
     public ApiResponse<CardInfoVo> getCardInfoByWalletAddress(ReqBigBottleQo qo) {
         //钱包地址
-        String walletAddress = qo.getWalletAddress().toLowerCase();
+        String walletAddress = qo.getWalletAddress();
+        if(StrUtil.isBlank(walletAddress)){
+            log.info("---> 缺失参数 walletAddress不能为空");
+            return ApiResponse.error(ResultCode.RECEIPT_ERR_PARAMETER_NOT_COMPLETE.getCode(), ResultCode.RECEIPT_ERR_PARAMETER_NOT_COMPLETE.getMessage());
+        }
+        walletAddress = walletAddress.toLowerCase();
 
         List<BVefutureBigBottle> bigBottles = getbVefutureBigBottles(walletAddress);
 
@@ -150,12 +155,12 @@ public class BVefutureBigBottleServiceImpl extends ServiceImpl<BVefutureBigBottl
 
         CardInfoVo cardInfoVo = new CardInfoVo();
         //钱包地址和图片地址
-        String walletAddress = qo.getWalletAddress().toLowerCase();
+        String walletAddress = qo.getWalletAddress();
         if(StrUtil.isBlank(walletAddress)){
             log.info("---> 缺失参数 walletAddress不能为空");
             return ApiResponse.error(ResultCode.RECEIPT_ERR_PARAMETER_NOT_COMPLETE.getCode(), ResultCode.RECEIPT_ERR_PARAMETER_NOT_COMPLETE.getMessage());
         }
-
+        walletAddress = walletAddress.toLowerCase();
         //当前本地时间
         List<BVefutureBigBottle> currentWeekBigBottles = getbVefutureBigBottles(walletAddress);
         if(CollectionUtil.isEmpty(currentWeekBigBottles)){
@@ -218,7 +223,7 @@ public class BVefutureBigBottleServiceImpl extends ServiceImpl<BVefutureBigBottl
     public ApiResponse processReceipt(ReqBigBottleQo reqBigBottleQo) {
 
         //钱包地址和图片地址
-        String walletAddress = reqBigBottleQo.getWalletAddress();
+        String walletAddress = reqBigBottleQo.getWalletAddress().toLowerCase();
         String imgUrl = reqBigBottleQo.getImgUrl();
         if(StrUtil.isBlank(walletAddress) || StrUtil.isBlank(imgUrl)){
             log.info("---> 缺失参数 walletAddress imgUrl都不能为空");
