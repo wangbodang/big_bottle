873c48d33eff2de4e06e44822c2265113fa89147
wangb <870719738@qq.com>
2025-04-18 15:31:04 +0900

    docs(custom): 数据表sql更正

--- DOCS/sql/b_vef_process_log.sql
+++ DOCS/sql/b_vef_process_log.sql
@@ -1,45 +1,68 @@
-create table vefuture.b_vef_process_log
+-- Table: vefuture.b_vef_process_log
+
+-- DROP TABLE IF EXISTS vefuture.b_vef_process_log;
+
+CREATE TABLE IF NOT EXISTS vefuture.b_vef_process_log
 (
-    id                     bigserial primary key,
-    process_id             char(36),
-    wallet_address         varchar(100) not null,
-    img_url                text         not null,
-    llm_type               varchar(100),
-    llm_start_time         timestamp(0) WITH TIME ZONE,
-    llm_end_time           timestamp(0) WITH TIME ZONE,
-    llm_return_msg         text,
+    id bigserial primary key,
+    process_id character(36) COLLATE pg_catalog."default",
+    wallet_address character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    img_url text COLLATE pg_catalog."default" NOT NULL,
+    ai_type character varying(100) COLLATE pg_catalog."default",
+    ai_process_start_time timestamp(0) with time zone,
+    ai_process_end_time timestamp(0) with time zone,
+    ai_return_msg text COLLATE pg_catalog."default",
+    remark text COLLATE pg_catalog."default",
+    is_delete boolean DEFAULT false,
+    create_id bigint,
+    create_time timestamp(0) with time zone DEFAULT now(),
+    update_id bigint,
+    update_time timestamp(0) with time zone DEFAULT now()
+)
 
-    remark                 text,
-    is_delete              boolean default false,
-    create_id              bigint,
-    create_time            timestamp(0) WITH TIME ZONE default now(),
-    update_id              bigint,
-    update_time            timestamp(0) WITH TIME ZONE default now()
+    TABLESPACE pg_default;
 
-);
+ALTER TABLE IF EXISTS vefuture.b_vef_process_log
+    OWNER to postgres;
 
-comment on table vefuture.b_vef_process_log is 'vefuture bigbottle 基本信息表';
+COMMENT ON COLUMN vefuture.b_vef_process_log.id
+    IS '主键, 序列自增';
 
-comment on column vefuture.b_vef_process_log.id is '主键长整型自增';
+COMMENT ON COLUMN vefuture.b_vef_process_log.process_id
+    IS '流程ID，全流程使用';
 
-comment on column vefuture.b_vef_process_log.wallet_address is '钱包地址';
+COMMENT ON COLUMN vefuture.b_vef_process_log.wallet_address
+    IS '钱包地址';
 
-comment on column vefuture.b_vef_process_log.img_url is '图片地址';
+COMMENT ON COLUMN vefuture.b_vef_process_log.img_url
+    IS '图片URL';
 
+COMMENT ON COLUMN vefuture.b_vef_process_log.ai_type
+    IS 'AI模型类型';
 
+COMMENT ON COLUMN vefuture.b_vef_process_log.ai_process_start_time
+    IS 'AI流程 开始时间';
 
-comment on column vefuture.b_vef_process_log.remark is '备注';
+COMMENT ON COLUMN vefuture.b_vef_process_log.ai_process_end_time
+    IS 'AI流程结束时间';
 
-comment on column vefuture.b_vef_process_log.create_id is '创建人ID';
+COMMENT ON COLUMN vefuture.b_vef_process_log.ai_return_msg
+    IS 'AI模型返回信息';
 
-comment on column vefuture.b_vef_process_log.create_time is '创建时间';
+COMMENT ON COLUMN vefuture.b_vef_process_log.remark
+    IS '备注';
 
-comment on column vefuture.b_vef_process_log.update_id is '修改人ID';
+COMMENT ON COLUMN vefuture.b_vef_process_log.is_delete
+    IS '删除标志, Boolean类型';
 
-comment on column vefuture.b_vef_process_log.is_time_threshold is '布尔值，用来检验小票时间和上传到系统时间的差值（比如我们规定小票时间是本周内的，就需要做这个判断）';
+COMMENT ON COLUMN vefuture.b_vef_process_log.create_id
+    IS '创建人ID';
 
-alter table vefuture.b_vef_process_log
-    owner to postgres;
+COMMENT ON COLUMN vefuture.b_vef_process_log.create_time
+    IS '创建时间';
 
-create index index_wallet_address
-    on vefuture.b_vef_process_log (process_id);
+COMMENT ON COLUMN vefuture.b_vef_process_log.update_id
+    IS '修改人ID';
+
+COMMENT ON COLUMN vefuture.b_vef_process_log.update_time
+    IS '修改时间';
